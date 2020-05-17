a05409de406eff6d5486ece9c970bda8a9eacd67
Scott <scott.r.ja@gmail.com>
2020-05-16 23:14:23 -0500

    init changed

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ from flask_wtf.csrf import CSRFProtect
 
 from flask import render_template
 app = Flask(__name__)
-SQLALCHEMY_DATABASE_URI ="postgresql://webdev:12345678@localhost/webDevProject2"
+# SQLALCHEMY_DATABASE_URI ="postgresql://webdev:12345678@localhost/webDevProject2"
+SQLALCHEMY_DATABASE_URI = "postgres://pyogevjdhhpdbe:dd8dca38e3a5ccb55e7e4eb4f1b0d2b1cb29a624ef71e182552480b86bdfdd6f@ec2-52-87-135-240.compute-1.amazonaws.com:5432/d7gshjrv8bbr12"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 UPLOAD_FOLDER = "./app/static/uploads"
 SECRET_KEY = 'Sup3r$3cretkey'
